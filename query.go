f003686ea71d5d701598fd9b2dfcbba7de85f80b
Guo Y.K <hi@guoyk.net>
2021-03-08 15:57:44 +0800

    Update query.go

--- query.go
+++ query.go
@@ -34,17 +34,50 @@ type QueryTransactionResponse struct {
 		Inputs      []struct {
 			Address string  `json:"ctaAddress"`
 			Amount  GetCoin `json:"ctaAmount"`
-			TxIndex int     `json:"ctaTxIndex"`
-		} `json:"ctxInputs"`
+			TxIndex *int    `json:"ctaTxIndex"`
+		} `json:"ctsInputs"`
 		Outputs []struct {
 			Address string  `json:"ctaAddress"`
 			Amount  GetCoin `json:"ctaAmount"`
-			TxIndex int     `json:"ctaTxIndex"`
-		} `json:"ctxOutputs"`
+			TxIndex *int    `json:"ctaTxIndex"`
+		} `json:"ctsOutputs"`
 	} `json:"Right"`
 }
 
 func (resp QueryTransactionResponse) Check() (err error) {
+	{
+		for _, ip := range resp.Right.Inputs {
+			if ip.Address == "" {
+				err = errors.New("input missing address")
+				return
+			}
+			if ip.Amount.Value == 0 {
+				err = errors.New("input missing amount")
+				return
+			}
+			if ip.TxIndex == nil {
+				err = errors.New("input missing ctaTxIndex")
+				return
+			}
+		}
+	}
+	{
+		for _, ip := range resp.Right.Outputs {
+			if ip.Address == "" {
+				err = errors.New("output missing address")
+				return
+			}
+			if ip.Amount.Value == 0 {
+				err = errors.New("output missing amount")
+				return
+			}
+			if ip.TxIndex == nil {
+				err = errors.New("output missing ctaTxIndex")
+				return
+			}
+		}
+	}
+
 	{
 		var ti int64
 		for _, ip := range resp.Right.Inputs {
@@ -69,58 +102,6 @@ func (resp QueryTransactionResponse) Check() (err error) {
 	return
 }
 
-/*
-{
-    "Right": {
-        "ctsId": "2d814167828c27f43148adb8afd09efeac99ccb6d4a2868d5961c0178d86d205",
-        "ctsTxTimeIssued": 1614564156,
-        "ctsBlockTimeIssued": 1614564156,
-        "ctsBlockHeight": 5403265,
-        "ctsBlockEpoch": 250,
-        "ctsBlockSlot": 361065,
-        "ctsBlockHash": "f6a0a7065b31ae99918120a3e3f35fc7aae3508cfa4b2484f410963f4a8f0be5",
-        "ctsRelayedBy": null,
-        "ctsTotalInput": {
-            "getCoin": "40983191022"
-        },
-        "ctsTotalOutput": {
-            "getCoin": "40983018305"
-        },
-        "ctsFees": {
-            "getCoin": "172717"
-        },
-        "ctsInputs": [
-            {
-                "ctaAddress": "DdzFFzCqrht6wUoxsseKN3bwqxaHrbQsUKzwMYA4KpMHoEbtyBRhnSXU71q4mj4KRJnssqAkHRw3V8kKSmTKzJdc1HJfjXJ94BsUdcyR",
-                "ctaAmount": {
-                    "getCoin": "40983191022"
-                },
-                "ctaTxHash": "95928b53aae97be6e8f39981ce6003ad7516136d8549e9091540d417870a872d",
-                "ctaTxIndex": 1
-            }
-        ],
-        "ctsOutputs": [
-            {
-                "ctaAddress": "Ae2tdPwUPEZHHcpw5Kz63Mh7SFapUfjNhjv9JXUZxukjc2AR1tUH92UMqpT",
-                "ctaAmount": {
-                    "getCoin": "17162323"
-                },
-                "ctaTxHash": "2d814167828c27f43148adb8afd09efeac99ccb6d4a2868d5961c0178d86d205",
-                "ctaTxIndex": 1
-            },
-            {
-                "ctaAddress": "DdzFFzCqrhseocHFPNhED2KXwgPMFkR28AWQTdTKYR95LitEVE1FSbT9hBh3592SU5WSENqt8bRYmW17qUCxCyBykKgrNKfoWMouGzy2",
-                "ctaAmount": {
-                    "getCoin": "40965855982"
-                },
-                "ctaTxHash": "2d814167828c27f43148adb8afd09efeac99ccb6d4a2868d5961c0178d86d205",
-                "ctaTxIndex": 0
-            }
-        ]
-    }
-}
-*/
-
 func QueryTransaction(endpoint string, txid string, txoidx int) (contributes map[string]int64, err error) {
 	contributes = make(map[string]int64)
 	var resp QueryTransactionResponse
@@ -133,7 +114,7 @@ func QueryTransaction(endpoint string, txid string, txoidx int) (contributes map
 	var ok bool
 	var lovelaceReceived int64
 	for _, out := range resp.Right.Outputs {
-		if out.TxIndex == txoidx {
+		if *out.TxIndex == txoidx {
 			lovelaceReceived = out.Amount.Value
 			ok = true
 			break
